81a161f567fb2b6a1005a61b0b11846a1618a6fc
Luke <gl.coin18@gmail.com>
2023-02-16 12:20:12 -0800

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* prints out programming is like building a mutilingual puzzel */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle);
 	getchar();
 	return 0;
 }

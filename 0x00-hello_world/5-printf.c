e5886e18ba23d8931071e549c3fc622146051cc0
Luke <gl.coin18@gmail.com>
2023-02-16 13:04:12 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prints out programming is like building a mutilingual puzzel'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void) /*main entry */
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}

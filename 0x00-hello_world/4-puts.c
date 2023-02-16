a5c652eb516b50f6f4970ef16a99c3439a82bb65
Luke <gl.coin18@gmail.com>
2023-02-16 12:14:07 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+/* prints out programming is like building a mutilingual puzzel */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	getchar();
+	return 0;
+}

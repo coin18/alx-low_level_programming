d6e8af6630bc5abd5a29d3abcb933dcee15b5884
Luke <gl.coin18@gmail.com>
2023-02-16 12:39:32 -0800

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<string.h>
-/* prints out programming is like building a mutilingual puzzel */
-int main(void)
+
+/**
+ * main - Entry point
+ *
+ * return: (success)
+ */
+int main(void) /*main entry */
 {
 	puts("\"Programming is like building a multilingual puzzle");
+/* prints out programming is like building a mutilingual puzzel */
 	getchar();
 	return (0);
 }

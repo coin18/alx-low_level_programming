3d786a055f10bc2386dfa6734945715a3b38a0ec
Luke <gl.coin18@gmail.com>
2023-02-17 12:50:56 -0800

    tebahpla 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * lowercase alphabet in reverse, 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}

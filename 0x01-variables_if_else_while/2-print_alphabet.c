5546508d239e12172c71d03c4d257a42c854b368
Luke <gl.coin18@gmail.com>
2023-02-17 12:33:10 -0800

    d

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,16 @@
  * Return: always (0)
  */
 
-int main(void) 
+int main(void)
 {
-	char low;
+	char c = 'a';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
 
 	return (0);
 }

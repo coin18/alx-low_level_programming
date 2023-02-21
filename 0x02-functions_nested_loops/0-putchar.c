f1c5b49e76d45ed6cc5a2441c29100db929cb403
luke <gl.coin18@gmail.com>
2023-02-21 12:50:46 -0800

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
-*main - prints putchar
-*
-*Return: 0 on success execution.
-*/
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
-

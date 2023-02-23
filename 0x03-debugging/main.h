d7931cd62f90964f42b0e71fb6bb008a3cfc9e0b
luke <gl.coin18@gmail.com>
2023-02-23 15:24:20 -0800

    2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
-postitive_or_negative(void);
+int postitive_or_negative(void);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
